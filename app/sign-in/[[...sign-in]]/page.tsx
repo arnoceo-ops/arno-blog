90990a4af5fdb385c59b92e6f90409576d89b8ee
Arno Diepeveen <arno@royaldutchsales.com>
2026-06-02 18:58:50 +0200

    fix: SignIn fallbackRedirectUrl naar /bot (Clerk v7)

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from '@clerk/nextjs'
 export default function SignInPage() {
   return (
     <div style={{ display: 'flex', minHeight: '100vh', alignItems: 'center', justifyContent: 'center', backgroundColor: '#0a0a0a' }}>
-      <SignIn />
+      <SignIn fallbackRedirectUrl="/bot" />
     </div>
   )
 }
